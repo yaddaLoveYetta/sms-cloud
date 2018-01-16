bd42f643846efde85ee54ada05f51ad6fb824f14
yadda <silenceisok@163.com>
2018-01-16 18:02:18 +0800

    add htdocs

---
+++ src/main/webapp/htdocs/api/demo/3.js
@@ -0,0 +1,20 @@
+﻿
+
+
+
+//可以生成很复杂的动态数据，并根据提交的参数进行处理。
+//具有真正模拟后台逻辑的能力。
+SMS.Proxy.response(function (data, config) {
+
+    return {
+
+        code: 206, //注意这里
+        msg: 'fail 206!!!!',
+        data: {
+            
+        }
+
+    }
+
+});
+
