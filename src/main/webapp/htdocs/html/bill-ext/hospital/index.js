47919da35bc1f87997c89a44b27cc6bd720c209e
yadda <silenceisok@163.com>
2018-04-17 18:33:33 +0800

    hospital info show edit

--- src/main/webapp/htdocs/html/bill-ext/hospital/index.js
+++ src/main/webapp/htdocs/html/bill-ext/hospital/index.js
@@ -9,13 +9,19 @@
     var $ = require('$');
     var MiniQuery = require('MiniQuery');
     var SMS = require('SMS');
+    var API = SMS.require('API');
     var Iframe = SMS.require('Iframe');
     var MessageBox = SMS.require('MessageBox');
-    var FormAction = require('FormAction');
-    var FormEdit = require('FormEdit');
-    var FileUpload = require('FileUpload');
-
     var BL = SMS.require('ButtonList');
+
+    var FormAction = require('FormAction');
+    var FileUpload = require('FileUpload');
+    var DatetimePicker = require('DatetimePicker');
+    var Selector = require('Selector');
+    var NumberField = require('NumberField');
+    var Address = require('Address');
+    var Edit = require('Edit');
+
 
     var user = SMS.Login.get();
     //检查登录状态
@@ -59,73 +65,86 @@
         // 自定义事件
         ButtonList.on('click', {
             'save': function (item, index) {
-                // 新增、修改时保存
-                FormEdit.save(function (ret) {
-
-                    if (operate === 1) {
-                        id = ret.value;
-                        operate = 2;
-                        FormEdit.render({
-                            classId: classId,
-                            id: id,
-                            operate: operate
-                        });
-                        // 向主控台跑出一个单据新增成功事件
-                        Iframe.raise({
-                            'eventName': 'addSuccess',
-                            'data': Iframe.getInfos()
-                        });
-                    }
+                // 修改时保存
+                Edit.save(function (ret) {
                     SMS.Tips.success("保存成功!", 1500);
                 });
-                console.log(item);
             },
             'refresh': function (item, index) {
                 // 刷新
-                FormEdit.render({
-                    classId: classId,
-                    id: id,
-                    operate: operate
-                });
+                Edit.render(operate, classId, id);
             }
         });
     });
 
-    FormEdit.on({
+    DatetimePicker.render();
+    NumberField.render();
+    Selector.render();
+    Edit.render(operate, classId, id);
+
+    Edit.on({
+        'numberFieldSet': function (field, key, value) {
+            NumberField.set(field, key, value);
+        },
+        'numberFieldGet': function () {
+            return NumberField.get(field, key);
+        },
+        'selectorSet': function (field, key, selectorData) {
+            Selector.set(field, key, selectorData);
+        },
         'afterFill': function (classId, metaData, data) {
-            if (classId === 1012) {
-                $("#hospital-logo").attr('src', data['logo']);
-            }
+            $("#hospital-logo").attr('src', data['logo']);
+            // 渲染地址选择器
+            Address.create({
+                'id': 'address-picker',
+                'province': data.province || -1,
+                'city': data.city || -1,
+                'district': data.district || -1
+            });
+            Address.showHeadValidInfo(false);
         },
         'afterInitPage': function (metaData) {
 
             if (operate === 0) {
                 $('#hospital-logo-select').remove();
-                return;
+            } else {
+
+                var api = new API("hospital/changeLogo");
+
+                FileUpload.render('#hospital-logo-select', {
+                    uploadUrl: api.getUrl(),// 上传请求路径
+                    uploadExtraData: function (previewId, index) {
+                        //额外参数 返回json数组
+                        return {
+                            classId: classId,
+                            id: id
+                        };
+                    }
+                }, function (fileNames, resp) {
+                    if (resp.code === 200) {
+                        $("#hospital-logo").attr('src', resp.data[fileNames[0]]);
+                    } else {
+                        SMS.Tips.error(resp.msg, 1000);
+                    }
+                });
             }
-            FileUpload.render('#hospital-logo-select', {
 
-                uploadExtraData: function (previewId, index) {
-                    //额外参数 返回json数组
-                    return {
-                        classId: classId,
-                        id: id
-                    };
-                }
-            }, function (fileNames, resp) {
-                if (resp.code === 200) {
-                    $("#hospital-logo").attr('src', resp.data[fileNames[0]]);
-                } else {
-                    SMS.Tips.error(resp.msg, 1000);
-                }
-            });
+        },
+        'beforeSave': function (metaData, headData) {
+            var address = Address.getSelectedItems();
+            if (address.length !== 3) {
+                Address.showHeadValidInfo(true);
+                return false;
+            } else {
+                Address.showHeadValidInfo(false);
+                headData.successData['province'] = address[0][0] || -1;
+                headData.successData['city'] = address[1][0] || -1;
+                headData.successData['district'] = address[2][0] || -1;
+
+                return true;
+            }
+
         }
-    });
-
-    FormEdit.render({
-        classId: classId,
-        id: id,
-        operate: operate
     });
 
 })(jQuery, MiniQuery, SMS);
