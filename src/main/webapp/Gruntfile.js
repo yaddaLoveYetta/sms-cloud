31adc852bb414a2c9d0029b814d02d4812af4f13
yadda <silenceisok@163.com>
2018-08-11 18:38:09 +0800

    grunt

--- src/main/webapp/Gruntfile.js
+++ src/main/webapp/Gruntfile.js
@@ -1,33 +1,12 @@
 module.exports = function (grunt) {
 
-    /*
-     * npm i grunt grunt-cli -g //安装grunt到全局
-     * npm i less@latest -g //安装less到全局
-     * 安装插件 -并保存依赖到package.json
-     * npm install grunt --save-dev
-     * npm install grunt-contrib-clean --save-dev   // 清理
-     * npm install grunt-contrib-copy --save-dev    // 复制
-     * npm install grunt-contrib-jshint --save-dev  //js语法检查
-     * npm install grunt-contrib-concat --save-dev    // 合并
-     * npm install grunt-contrib-uglify --save-dev //  js压缩
-     * npm install grunt-contrib-csslint --save-dev  // css语法检查
-     * npm install grunt-contrib-cssmin --save-dev // css压缩
-     * npm install grunt-filerev --save-dev // 文件重命名
-     * npm install grunt-contrib-watch --save-dev  // 检测文件变化
-     * npm install grunt-usemin --save-dev // 修改html对js，css的引用
-    */
-
     var path = {
         src: 'htdocs',
         dest: 'build',
         tmp: '.tmp'
     };
 
-    // 所有插件的配置信息
     grunt.initConfig({
-
-        // 读取package.json文件信息
-        pkg: grunt.file.readJSON('package.json'),
 
         path: path,
 
@@ -35,12 +14,19 @@ module.exports = function (grunt) {
         clean: {
             beforeBuild: {
                 files: [{
-                    src: ['<%= path.dest %>/']
+                    src: ['<%= path.dest %>/', '<%= path.tmp %>/']
+                }
+                ]
+            },
+            afterBuild: {
+                files: [{
+                    src: ['<%= path.tmp %>/']
                 }
                 ]
             }
         },
 
+        // 复制文件，后续合并压缩都在目标文件中进行，不操作源代码文件
         copy: {
             src: {
                 files: [
@@ -57,99 +43,129 @@ module.exports = function (grunt) {
             }
         },
 
-        concat: {
-            options: {
-                // 合并文件之间分隔符号
-                separator: ';',
-                // 合并时允许输出头部信息
-                stripBanners: true,
-                // 头部注释
-                banner: '/*!<%= pkg.name %> - <%= pkg.version %>-' + '<%=grunt.template.today("yyyy-mm-dd") %> */'
-            },
-            dist: {
-                src: ['src/zepto.js', 'src/underscore.js', 'src/backbone.js'],
-                dest: 'dest/libs.js'
-            }
-        },
-
-        //uglify压缩js插件配置信息
-        uglify: {
-
-            options: {
-                // 去掉注释
-                stripBanners: true,
-                // 在压缩文件头部加上注释
-                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> start... */\n',
-                //在压缩文件底部加上注释
-                footer: '\n/*! <%= pkg.name %> 最后修改于： <%= grunt.template.today("yyyy-mm-dd") %> */',
-                //不混淆函数名和变量名 默认会混淆
-                mangle: false,
-                //输出压缩率，可选的值有 false(不输出信息)，gzip
-                report: "min",
-                //不删除注释，还可以为 false（删除全部注释），some（保留@preserve @license @cc_on等注释）
-                preserveComments: 'all'
-
-            },
-
-            // 批量压缩
-            batch: {
-                //将占位符*展开 即使用占位符匹配文件名
-                expand: true,
-                cwd: '<%= path.dest %>/',
-                //压缩src目录及所有子目录下的js文件
-                src: ['**/*.js', '!**/*min.js'],
-                //压缩文件存放到build 目录下的同名目录
-                dest: '<%= path.dest %>/',
-                //压缩文件的后缀名
-                ext: '.min.js'
-            }
-        },
-
-        //css压缩插件配置信息
-        cssmin: {
-            build: {
-                files: [{
-                    //为true启用cwd,src,dest选项
-                    expand: true,
-                    //所有src指定的匹配都将相对于此处指定的路径（但不包括此路径）
-                    cwd: '<%= path.dest %>/',
-                    //相对于cwd路径的匹配模式。意思就是 src/**/*.css，匹配src下面所有文件
-                    src: ['**/*.css', '!**/*min.css'],
-                    //目标文件路径前缀。
-                    dest: '<%= path.dest %>/',
-                    //压缩文件的后缀名
-                    ext: '.min.css'
-                }
-                ]
-            }
-        },
-
-        //对css和js文件重命名
+        // 对css和js文件重命名
         filerev: {
             build: {
                 files: [{
-                    src: ['<%= path.dest %>/**',
-                        '!<%= path.dest %>/**/*.html',//html文件不加版本号
-                        '!<%= path.dest %>/**/*.{png,jpg,jpeg}']//图片 不需要加版本号
+                    src: ['<%= path.dest %>/**', '!<%= path.dest %>/**/*.html']
                 }
                 ]
             }
         },
+
         //替换文件前准备
         useminPrepare: {
-            html: '<%= path.dest %/**/*.html'
-            /*            options: {
-                            dest: './<%= path.dest %>',
-                            root: './<%= path.dest %>'
-                        }*/
-        },
 
-        //修改html中的css和js引用
-        usemin: {
-            html: {
+            build: {
                 files: [{
                     src: '<%= path.dest %>/**/*.html'
                 }
+                ]
+            },
+            options: {
+                // 定义临时文件夹
+                staging: '<%= path.tmp %>',
+                // 定义目标文件夹
+                dest: '<%= path.dest %>',
+                // 默认是源html所在的文件夹，定义处理的资源文件的相对路径,concat:generated将从root开始找文件
+                root: '<%= path.dest %>',
+                flow: {
+                    build: {
+                        steps: {
+                            //注意不是uglify，是uglifyjs，下面的post中对应的name为uglify
+                            js: ['concat', 'uglifyjs'],
+                            css: ['concat', 'cssmin']
+                        },
+                        post: {
+                            js: [{
+                                name: 'concat',
+                                createConfig: function (context, block) {
+                                    //context.options即concat task
+                                    var generated = context.options.generated;
+                                    generated.options = {
+                                        // 合并文件之间的分隔符
+                                        separator: ';',
+                                        //合并时是否删除源文件头部的注释，默认false
+                                        stripBanners: false,
+                                        // 合并文件头注释
+                                        banner: '/*concat by yadda use grunt==> begin===<%= grunt.template.today("yyyy-mm-dd HH:mm:ss") %>*/\n',
+                                        // 合并文件尾注释
+                                        footer: '/*concat by yadda use grunt==> end===<%= grunt.template.today("yyyy-mm-dd HH:mm:ss") %>*/\n',
+                                        sourceMap: true
+                                    };
+                                }
+                            }, {
+                                name: 'uglify',
+                                createConfig: function (context, block) {
+                                    //context.options即concat task
+                                    var generated = context.options.generated;
+                                    generated.options = {
+                                        // 不混淆代码
+                                        mangle: false,
+                                        // 可理解为保留换行
+                                        beautify: true,
+                                        // 保留注释相关，接受false、'all'、'some'、Function
+                                        preserveComments: true,
+                                        sourceMap: true,
+                                        // 输出压缩率
+                                        report: "min",
+                                        // 压缩文件头注释
+                                        banner: '/*concat-uglify by yadda use grunt==> begin===<%= grunt.template.today("yyyy-mm-dd HH:mm:ss") %>*/\n',
+                                        // 压缩文件尾注释
+                                        footer: '/*concat-uglify by yadda use grunt==> end===<%= grunt.template.today("yyyy-mm-dd HH:mm:ss") %>*/\n',
+                                        // 压缩
+                                        compress: {
+                                            // 去除console语句
+                                            drop_console: true
+                                        }
+                                    };
+                                }
+                            }],
+                            css: [{
+                                name: 'concat',
+                                createConfig: function (context, block) {
+                                    //context.options即concat task
+                                    var generated = context.options.generated;
+                                    generated.options = {
+                                        // 合并文件之间的分隔符
+                                        separator: ';',
+                                        //合并时是否删除源文件头部的注释，默认false
+                                        stripBanners: false,
+                                        // 合并文件头注释
+                                        banner: '/*concat by yadda use grunt==> begin===<%= grunt.template.today("yyyy-mm-dd HH:mm:ss") %>*/\n',
+                                        // 合并文件尾注释
+                                        footer: '/*concat by yadda use grunt==> end===<%= grunt.template.today("yyyy-mm-dd HH:mm:ss") %>*/\n',
+                                        sourceMap: true
+                                    };
+                                }
+                            }, {
+                                name: 'cssmin',
+                                createConfig: function (context, block) {
+                                    //context.options即concat task
+                                    var generated = context.options.generated;
+                                    generated.options = {
+
+                                        sourceMap: true,
+                                        // 输出压缩率
+                                        report: "min"
+                                    };
+                                }
+                            }]
+                        }
+                    }
+                }
+            }
+        },
+
+        usemin: {
+            html: {
+                files: [
+                    {
+                        expand: true,
+                        cwd: '<%= path.dest %>',
+                        src: ['**/*.html'],
+                        dest: '<%= path.dest %>'
+                    }
                 ]
             }
         }
@@ -170,7 +186,5 @@ module.exports = function (grunt) {
     // 加载usemin插件
     grunt.loadNpmTasks('grunt-usemin');
 
-    // 告诉grunt在终端输入grunt时做什么任务(默认任务)任务按顺序执行
-    //grunt.registerTask('default', ['clean', 'copy', 'useminPrepare', 'usemin']);
-    grunt.registerTask('default', ['clean', 'copy', 'uglify', 'cssmin', 'useminPrepare', 'usemin']);
+    grunt.registerTask('default', ['clean:beforeBuild', 'copy', 'useminPrepare', 'concat:generated', 'cssmin:generated', 'uglify:generated', 'usemin']);
 };
